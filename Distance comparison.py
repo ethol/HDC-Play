ee9779cc5ff03b8bde666a79046819f4a978a5e6
tomglove <mrethol@gmail.com>
2023-10-13 13:38:19 +0200

    extended metrics Distance digits

--- Distance comparison.py
+++ Distance comparison.py
@@ -27,11 +27,11 @@ heatmap = {
 }
 count_heatmap = np.zeros((10, 10), dtype="int")
 
-for i in range(0, len(digits.target)):
+for i in range(0, int(len(digits.target)/1)):
     for j in range(i + 1, len(digits.target)):
         heatmap[digits.target[i]][digits.target[j]].append(
             # spatial.distance.euclidean(digits.data[i], digits.data[j]))
-        1 - spatial.distance.cosine(digits.data[i], digits.data[j]))
+            1 - spatial.distance.cosine(digits.data[i], digits.data[j]))
         count_heatmap[digits.target[i]][digits.target[j]] += 1
     if i % 100 == 0:
         print(i / len(digits.target), time.time() - start)
@@ -41,8 +41,18 @@ avg_heatmap = np.zeros((10, 10), dtype="float")
 
 for i in range(0, 10):
     for j in range(0, 10):
-        avg_heatmap[i][j] = sum(heatmap[i][j])/ count_heatmap[i][j]
+        avg_heatmap[i][j] = sum(heatmap[i][j]) / count_heatmap[i][j]
 
+print("avg distance between digits", np.sum(avg_heatmap) / 100)
+sorted_avg = avg_heatmap.flatten()
+sorted_avg.sort()
+
+for i in range(10):
+    loc = np.where(avg_heatmap == sorted_avg[i])
+    print(f'{loc[0]}:{loc[1]} = {sorted_avg[i]}')
+
+print(sorted_avg)
+print(avg_heatmap)
 
 ax = sns.heatmap(avg_heatmap, linewidth=0.5)
 plt.show()
